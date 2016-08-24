5dd36cac828ed335649bd2d25a099ad5af0bfc3d
AdamUnger <adamloganunger@gmail.com>
2016-08-24 19:23:42 -0400

    added starter dict for fav musicians

--- favorites.py
+++ favorites.py
@@ -1,6 +1,11 @@
 
 
 def main():
-	print 'Hello, World!'
+	favorite_musicians = {
+		'Adam': 'The Beatles',
+	}
+
+	for person, musician in favorite_musicians.items():
+		print "{person}'s favorite musician is {musician}".format(person=person, musician=musician)
 
 main()
